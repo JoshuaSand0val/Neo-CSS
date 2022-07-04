7eda2db3dc3e16a1fe730290e30aaf11ff188b2b
Joshua Sandoval <joshuaisgoogling@gmail.com>
2022-07-04 10:05:27 -0600

    Minor change to comment to be more accurate.

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
 	// Add code syntax highlighting plugin.
 	eleventyConfig.addPlugin(syntaxHighlight);
-	// Add passthrough copies of production asset files.
+	// Add passthrough copy of website asset files.
 	eleventyConfig.addPassthroughCopy("assets/");
 	// Add year shortcode, used for copyright.
 	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
