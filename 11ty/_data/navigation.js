a9125d6b52e891a0ad377b88c5129e04629e26ff
Joshua Sandoval <joshuaisgoogling@gmail.com>
2022-11-15 16:16:20 -0700

    Merged navigation data files.

---
+++ 11ty/_data/navigation.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "header": {
+        "Home": "/",
+        "API": "/api/",
+        "Github": "https://github.com/JoshuaSand0val/Neo-CSS/"
+    },
+    "footer": {
+        "Neo CSS Boilerplate": "/",
+        "Neo CSS API": "/api/"
+    }
+};
